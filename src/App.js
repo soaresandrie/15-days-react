b7168922b3b3d141d4df84b98f563af4ae320f62
jnegri <jandriesoares@gmail.com>
2019-04-12 16:10:21 -0300

    Exchange component of a statefull for stateless.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Go 15 days of React!!!</h1>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <h1>Go 15 days of React!!!</h1>
+//     );
+//   }
+// }
+import React from 'react'
+
+const App = () => <h1>Go 15 days of React</h1>
 
 export default App;
